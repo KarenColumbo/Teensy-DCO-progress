d86e356f509a5e1f79e90dbca51a6f4c83990b82
KarenColumbo <office@mrscolumbo.com>
2023-02-18 17:19:11 +0100

    dual channel & waveform switch

--- src/main.cpp
+++ src/main.cpp
@@ -107,7 +107,7 @@ void debugPrint(int voice) {
   //Serial.println(bendFactor);
 }
 
-void AD9833setFrequency(int board, long frequency0, long frequency1) {
+/*void AD9833setFrequency(int board, long frequency0, long frequency1) {
   long FreqReg0 = (frequency0 * pow(2, 28)) / MCLK;   // Data sheet Freq Calc formula
   int MSB0 = (int)((FreqReg0 & 0xFFFC000) >> 14);     // only lower 14 bits are used for data
   int LSB0 = (int)(FreqReg0 & 0x3FFF);
@@ -137,8 +137,58 @@ void AD9833setFrequency(int board, long frequency0, long frequency1) {
 
   digitalWrite(FSYNC_SET_PIN, HIGH);                        // write done, set FSYNC high
   SPI.endTransaction();
-}
+}*/
+void AD9833setFrequency(int board, long frequency0, long frequency1, int waveform1) {
+  long FreqReg0 = (frequency0 * pow(2, 28)) / MCLK;   // Data sheet Freq Calc formula
+  int MSB0 = (int)((FreqReg0 & 0xFFFC000) >> 14);     // only lower 14 bits are used for data
+  int LSB0 = (int)(FreqReg0 & 0x3FFF);
+  long FreqReg1 = (frequency1 * pow(2, 28)) / MCLK;
+  int MSB1 = 0;
+  int LSB1 = 0;
+  if (frequency1 > 0) {
+    MSB1 = (int)((FreqReg1 & 0xFFFC000) >> 14);     // only lower 14 bits are used for data
+    LSB1 = (int)(FreqReg1 & 0x3FFF);
+  }
+  int FSYNC_SET_PIN = FSYNC_PINS[board];
+  SPI.beginTransaction(SPISettings(SPI_CLOCK_SPEED, MSBFIRST, SPI_MODE2));
+  digitalWrite(FSYNC_SET_PIN, LOW);                         // set FSYNC low before writing to AD9833 registers
 
+  LSB0 |= 0x4000;                                    // DB 15=0, DB14=1
+  MSB0 |= 0x4000;                                    // DB 15=0, DB14=1
+  SPI.transfer16(LSB0);                              // write lower 16 bits to AD9833 registers
+  SPI.transfer16(MSB0);                              // write upper 16 bits to AD9833 registers
+
+  if (frequency1 > 0) {
+    LSB1 |= 0x4000;                                    // DB 15=0, DB14=1
+    MSB1 |= 0x4000;                                    // DB 15=0, DB14=1
+    switch (waveform1) {
+      case 1:
+        MSB1 |= 0x2000;  // set DB13=1 for square wave
+        break;
+      case 2:
+        // set waveform 2 (sine wave), do nothing here as it is the default waveform
+        break;
+      case 3:
+        MSB1 |= 0x2020;  // set DB13=1 and DB5=1 for triangle wave
+        break;
+      case 4:
+        MSB1 |= 0x800;   // set DB11=1 for half-square wave
+        break;
+      default:
+        // invalid waveform type, do nothing here as it is the default waveform
+        break;
+    }
+    SPI.transfer16(LSB1);                              // write lower 16 bits to AD9833 registers
+    SPI.transfer16(MSB1);                              // write upper 16 bits to AD9833 registers
+  }
+  
+  SPI.transfer16(0xC000);                           // write phase register
+  SPI.transfer16(0x2002);                           // take AD9833 out of reset and output triangle wave (DB8=0)
+  delayMicroseconds(2);                             // Settle time? Doesn't make much difference …
+
+  digitalWrite(FSYNC_SET_PIN, HIGH);                        // write done, set FSYNC high
+  SPI.endTransaction();
+}
 
 void AD9833Reset(int AD_board) {
   SPI.beginTransaction(SPISettings(SPI_CLOCK_SPEED, MSBFIRST, SPI_MODE2));
