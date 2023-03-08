5b56e25a4b8edad0bb9277714ef70e51b6e98ee5
KarenColumbo <office@mrscolumbo.com>
2023-03-08 14:52:54 +0100

    lib update

--- include/PDM-lib.h
+++ include/PDM-lib.h
@@ -20,18 +20,8 @@ class PDM {
     static void outputPDMInterrupt();
 };
 
+void outputVoices();
+void stopOutput();
+float calculateVoltageFromFreq(float freq);
+
 #endif
-
-/*
-This implementation defines a PDM class with the following public methods:
-
-    PDM(uint8_t pin, float voltage): constructor that takes the GPIO pin and output voltage as arguments and initializes the PDM parameters.
-    void enable(): method that enables the PDM output on the specified GPIO pin.
-    void disable(): method that disables the PDM output.
-
-The private members of the PDM class include the PDM sample rate, output bit depth, output period, pulse width, silence width, output pin, enable flag, counter variable, and the interrupt service routine (outputPDMInterrupt) that is called by the IntervalTimer object to output the PDM signal on the specified GPIO pin.
-
-To use the PDM library, you can simply include the PDM-Lib.h header file in your program and create a PDM object with the desired GPIO pin and output voltage. Then, you can call the enable method to start the PDM output and the disable method to stop the output when necessary.
-
-Here's an example implementation of a program that uses the PDM library to output PDM signals for multiple voices with polyphony:
-*/
